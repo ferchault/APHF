c00fb579f76239691f392b1deee5e086fe0a72f4
von Rudorff, Guido Falk <guido@vonrudorff.de>
2021-07-28 15:00:21 +0200

    H2 test

--- pyscf-test.py
+++ pyscf-test.py
@@ -9,3 +9,8 @@ mol = gto.M(atom=f"H {a} 0 0; H {-a} 0 0; O 0 {b} 0", basis="sto-3g")
 calc = scf.RHF(mol)
 calc.kernel()
 print(calc.mo_energy)
+
+mol = gto.M(atom=f"H 0 0 0; H {1.4/1.8897259886} 0 0", basis="sto-3g")
+calc = scf.RHF(mol)
+calc.kernel()
+print(calc.mo_energy)
