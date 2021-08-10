1b8db60a06efeeced250856190a9f90fda63e01e
Guido Falk von Rudorff <guido@vonrudorff.de>
2021-08-10 16:26:16 +0200

    Analysis of groups

--- convergence.py
+++ convergence.py
@@ -4,6 +4,7 @@ import numpy as np
 import configparser
 import scipy.interpolate as sci
 import mpmath
+import pandas as pd
 
 #%%
 def padesplit(coeffs):
@@ -49,11 +50,25 @@ class Calculation:
     def _read_data(self):
         self._update_accuracy()
 
-        self._data = {"energy": {}}
+        self._data = {"energy_0": {}}
         for label, value in self._config["singlepoints"].items():
             parts = label.split("_")
             if parts[0] == "energy":
-                self._data["energy"][parts[1]] = value
+                self._data["energy_0"][parts[1]] = value
+
+            if parts[0] == "moenergy":
+                _, offset, moid = parts
+                key = f"moenergy_{moid}"
+                if key not in self._data:
+                    self._data[key] = {}
+                self._data[key][offset] = value
+
+            if parts[0] == "dm":
+                _, offset, i, j = parts
+                key = f"dm_{i}_{j}"
+                if key not in self._data:
+                    self._data[key] = {}
+                self._data[key][offset] = value
 
     def get_target(self, key):
         return self._data[key]["target"]
@@ -91,15 +106,23 @@ class Calculation:
 
         return float(self._config["singlepoints"]["energy_target"])
 
+    def get_keys_by_group(self, group):
+        return [_ for _ in self._data.keys() if _.startswith(f"{group}_")]
 
-for bs in "STO3G STO6G def2TZVP ccpvdz def2SVP 631G".split():
+
+group = "energy"
+for bs in "STO3G".split():  # STO6G def2TZVP ccpvdz def2SVP 631G
     c = Calculation(f"PROD/H2/dps-1000-{bs}.out")
 
-    target = float(c.get_target("energy"))
-    coeffs = np.array([float(_) for _ in c.get_coefficients("energy")])
-    xs, ys = padesplit(coeffs)
-    plt.semilogy(xs, abs(ys - target), label=bs)
-plt.legend()
+    rows = []
+    for key in c.get_keys_by_group(group):
+        target = float(c.get_target(key))
 
+        coeffs = np.array([float(_) for _ in c.get_coefficients(key)])
+        xs, ys = padesplit(coeffs)
+        for x, y in zip(xs, abs(ys - target)):
+            rows.append({"order": x, "error": y})
 
-# %%
+    df = pd.DataFrame(rows).groupby("order").mean().reset_index()
+
+    plt.semilogy(df.order, df.error)
