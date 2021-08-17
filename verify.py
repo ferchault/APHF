7a72755181f1f4dc64a5f8bafcd4f8a0099a0777
von Rudorff, Guido Falk <guido@vonrudorff.de>
2021-08-17 08:12:11 +0200

    Include target verification

--- verify.py
+++ verify.py
@@ -7,6 +7,7 @@ import sys
 from pyscf.gto import basis
 import pyscf.scf
 import pyscf.gto
+import pyscf.qmmm
 import configparser
 import basis_set_exchange as bse
 
@@ -43,9 +44,10 @@ def build_system(filename):
     atomspec = ""
     basisspec = {}
     idx = 0
-
+    deltaZ = []
     for ref, tar, bas, coord in zip(reference_Zs, target_Zs, basis_Zs, coords):
         N += int(ref)
+        deltaZ.append(int(tar) - int(ref))
         try:
             ref = bse.lut.element_data_from_Z(int(ref))[0].capitalize()
         except:
@@ -59,17 +61,25 @@ def build_system(filename):
         idx += 1
 
     mol = pyscf.gto.M(atom=atomspec, basis=basisspec, verbose=0)
+    calc_target = add_qmmm(pyscf.scf.RHF(mol), mol, deltaZ)
 
     eref = float(config["singlepoints"]["energy_0"])
     etarget = float(config["singlepoints"]["energy_target"])
-    return mol, eref, etarget
+    return mol, eref, etarget, calc_target
 
 
-mol, eref, etarget = build_system(sys.argv[1])
+if __name__ == "__main__":
+    mol, eref, etarget, calc_target = build_system(sys.argv[1])
 
-calc = pyscf.scf.RHF(mol)
-calc.kernel()
-if abs(calc.energy_elec()[0] - eref) < 1e-8:
-    print("OK", sys.argv[1])
-else:
-    print("EE", sys.argv[1], "pyscf", calc.energy_elec()[0], "fd", eref)
+    calc = pyscf.scf.RHF(mol)
+    calc.kernel()
+    calc_target.kernel()
+    if abs(calc.energy_elec()[0] - eref) < 1e-6:
+        print("OK ref", sys.argv[1])
+    else:
+        print("EE ref", sys.argv[1], "pyscf", calc.energy_elec()[0], "fd", eref)
+
+    if abs(calc_target.energy_elec()[0] - etarget) < 1e-6:
+        print("OK tar", sys.argv[1])
+    else:
+        print("EE tar", sys.argv[1], "pyscf", calc.energy_elec()[0], "fd", etarget)
